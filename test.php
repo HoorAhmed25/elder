79a5570548dde764df69aec9bb19806a8f315cb1
HoorAhmed25 <42851407+HoorAhmed25@users.noreply.github.com>
2022-04-10 21:21:24 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    </head>
+<body>
+    <form action="">
+   <select name="xray" id="p9" class="form-select w-75 form-control"  required>
+                        <option value="">--اختر--</option>
+                        <option value="نعم" >طبيعي</option>
+                        <option value="لا">غير طبيعي</option>
+                        <option value="لم يتم">لم يتم </option>
+                    </select>  
+    
+    <button class="btn btn-info" type="submit">submit</button>
+    </form>
+    </body>
+</html>
